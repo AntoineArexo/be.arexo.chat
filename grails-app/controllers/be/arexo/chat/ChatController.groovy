269f74be269f0ea8c0f5310a32ea59789ef62d49
Simon Lacroix <simon.lacroix@arexo.be>
2013-02-12 11:30:40 +0100

    Added logic to ChatController

--- grails-app/controllers/be/arexo/chat/ChatController.groovy
+++ grails-app/controllers/be/arexo/chat/ChatController.groovy
@@ -2,10 +2,26 @@ package be.arexo.chat
 
 class ChatController {
 	def index() {
-		
+		if (session?.user) {
+			redirect(action: "messages")
+		}
 	}
 	
 	def messages() {
-		
+		if (!session?.user) {
+			redirect(action: "index")
+		}
+		[user: session.user]
+	}
+	
+	def login() {
+		def username = params.username
+		def u = User.findByName(username)
+		if (!u) {
+			u = new User(username: username)
+			u.save()
+		}
+		session.user = u
+		redirect(action: "messages")
 	}
 }
