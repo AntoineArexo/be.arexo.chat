e3895b97a50546dfcb5b744374161ab123608ce8
Nicolas Collard <nicolas.collard@arexo.be>
2013-02-14 11:23:35 +0100

    Added save, update and delete method to PostController

--- grails-app/controllers/be/arexo/chat/CommentController.groovy
+++ grails-app/controllers/be/arexo/chat/CommentController.groovy
@@ -20,6 +20,6 @@ class CommentController {
         
     }
     def save() {
-        
+    	
     }
 }
--- grails-app/controllers/be/arexo/chat/PostController.groovy
+++ grails-app/controllers/be/arexo/chat/PostController.groovy
@@ -13,6 +13,34 @@ class PostController {
 			def all = Post.list()
 			render all as JSON
 		}
-		
+	}
+	
+	def update(){
+		def id = params.id
+		if(id && Post.exists(id)){
+			def p = Post.get(id)
+			p.title = params.title
+			p.text = params.text
+			
+			if(p.save()){
+				render p as JSON
+			}	
+		}
+	}
+	
+	def save(){
+		def p = new Post(params.title, params.text)
+		if(p.save()){
+			render p as JSON
+		}
+	}
+	
+	def delete(){
+		def id = params.id
+		if(id && Post.exists(id)){
+			def p = Post.get(id)
+			
+			p.delete(flush:true)
+		}
 	}
 }
