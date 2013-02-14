53ac1f24749c88abe527e38b3484aaf87f148697
Simon Lacroix <simon.lacroix@arexo.be>
2013-02-14 12:03:54 +0100

    Added time in Post and Comment

--- grails-app/domain/be/arexo/chat/Post.groovy
+++ grails-app/domain/be/arexo/chat/Post.groovy
@@ -1,9 +1,12 @@
 package be.arexo.chat
 
+import org.joda.time.DateTime
+
 class Post {
     
     String title
     String text
+    DateTime postTime
     
     static hasMany = [comments: Comment]
 
