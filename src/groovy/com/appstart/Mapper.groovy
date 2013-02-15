54887823f681a15185930875bcb60b3c4240b8cf
Simon Lacroix <simon.lacroix@arexo.be>
2013-02-15 17:22:28 +0100

    Added comment in Mapper

--- src/groovy/com/appstart/Mapper.groovy
+++ src/groovy/com/appstart/Mapper.groovy
@@ -5,19 +5,25 @@ class Mapper {
         def map = [:]
         def dependencies = [:]
         
+        // We collect map of element(s)
         if (o instanceof Collection) {
-            map[o[0].getRootName(true, options)] = o.collect{ 
+            map[o[0].getRootName(true, options)] = o.collect{
+                // We save the dependencies of each element
                 dependencies = mergeMap(dependencies, it.mapDependencies(true, options))
+                // We collect map of each element
                 it.transformToMap(true, options)
             }
         }
         else {
+            // If only one element, we get the map and dependencies
             map[o.getRootName(false, options)] = o.transformToMap(false, options)
             dependencies = mergeMap(dependencies, o.mapDependencies(false, options))
         }
         
+        // We collect map of dependencies
         dependencies.each{key, value ->
             def tmpMap = getMap(key.getAll(value), options)
+            // We add map of dependencies to the current map
             map = mergeMap(map, tmpMap)
         }
         
