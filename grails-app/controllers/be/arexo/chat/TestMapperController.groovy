73e83bdbc5d941a8d363bf403e4180c802f35e86
Simon Lacroix <simon.lacroix@arexo.be>
2013-02-15 16:13:47 +0100

    Clean + static Mapper

--- grails-app/controllers/be/arexo/chat/TestMapperController.groovy
+++ grails-app/controllers/be/arexo/chat/TestMapperController.groovy
@@ -8,17 +8,16 @@ class TestMapperController {
 
 	def index(){
 		def id = params.id
-		def mapper = new Mapper()
 		if(id && Post.exists(id)){
 			def post = Post.get(id)	
-			 render mapper.getMap(post, [:]) as JSON
+			 render Mapper.getMap(post, [:]) as JSON
 		}
 		else{
 			def all = Post.list()
 			//render all as JSON
 			
 			
-            render mapper.getMap(all, [:]) as JSON
+            render Mapper.getMap(all, [:]) as JSON
 			
 		}
 	}
--- src/groovy/com/appstart/Mapper.groovy
+++ src/groovy/com/appstart/Mapper.groovy
@@ -1,13 +1,13 @@
 package com.appstart
 
 class Mapper {
-    def getMap(Object o, Map options) {
+    static def getMap(Object o, Map options) {
         def map = [:]
         def dependencies = [:]
         
         if (o instanceof Collection) {
             map[o[0].getRootName(true, options)] = o.collect{
-                dependencies = mergeMap(dependencies, [:])//it.mapDependencies(true, options))
+                dependencies = mergeMap(dependencies, it.mapDependencies(true, options))
                 it.transformToMap(true, options)
             }
         }
@@ -20,42 +20,11 @@ class Mapper {
             def tmpMap = getMap(key.getAll(value), options)
             map = mergeMap(map, tmpMap)
         }
-//        
-//        dependencies.each{key, value ->
-//            if (key.getClass()) {
-//                if (value instanceof Collection) {
-//                    value.each{
-//                        key.get(it).transformToMap(true, options)
-//                    }
-//                }
-//                else {
-//                    map[key.get(value).getRootName(false, options)] = key.get(value).transformToMap(false, options)
-//                }
-//            }
-//        }
-//        
+        
         return map
     }
     
-    def mergeMap(Map one, Map two) {
+    static private def mergeMap(Map one, Map two) {
         return one + two
     }
-    
-    // Useless (I think)
-//    def parseToMap(map) {
-//        if (map instanceof Collection) {
-//            return map.collect{parseToMap(it)}
-//        }
-//        if (map instanceof Map) {
-//            def newMap = [:]
-//            map.each{key, val ->
-//                if (val instanceof Map || val instanceof Collection) {
-//                    val = parseToMap(val)
-//                }
-//                newMap[key] = val
-//            }
-//            return newMap
-//        }
-//        return map;
-//    }
 }
